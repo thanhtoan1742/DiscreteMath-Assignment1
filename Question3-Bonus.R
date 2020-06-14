f0435d91d91515615305036fc08fb0ef2e3ebbab
thanhtoan1742 <thanhtoan1742@gmail.com>
2020-06-14 20:54:37 +0700

    deleted:    Data/01.csv 	modified:   Question3-Bonus.R

--- Question3-Bonus.R
+++ Question3-Bonus.R
@@ -1,5 +1,13 @@
-library(xlsx)
-library(dplyr)
+options(encoding = "utf-8");
+library(readxl);
+library(xlsx);
+library(dplyr);
 
 #Read the Excel file
-thienan <- read.xlsx2("1.xlsx", sheetIndex = 1)
+data = data.frame();
+data = read.xlsx("Data\\1.xlsx", sheetIndex = 1);
+names(data) = NULL;
+row.names(data) = NULL;
+Encoding(data[, 2]) = "UTF-8";
+Encoding(data[, 5]) = "UTF-8";
+print(data);
