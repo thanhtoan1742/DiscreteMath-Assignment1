c4a4b491374b7b744204b8a14260901007048100
thanhtoan1742 <thanhtoan1742@gmail.com>
2020-06-14 21:27:17 +0700

    UTF-8 IO

--- Question3-Bonus.R
+++ Question3-Bonus.R
@@ -1,13 +1,14 @@
 options(encoding = "utf-8");
-library(readxl);
 library(xlsx);
-library(dplyr);
 
 #Read the Excel file
 data = data.frame();
 data = read.xlsx("Data\\1.xlsx", sheetIndex = 1);
-names(data) = NULL;
+namesList = c("Mã số ID", "Tình trạng", "Đã bắt đầu vào lúc", "Đã hoàn thành", "Thời gian thực hiện", "Điểm/10,00", "Q. 1 /1,00", "Q. 2 /1,00", "Q. 3 /1,00", "Q. 4 /1,00", "Q. 5 /1,00", "Q. 6 /1,00", "Q. 7 /1,00", "Q. 8 /1,00", "Q. 9 /1,00", "Q. 10 /1,00");
+Encoding(namesList) = "UTF-8";
+names(data) = namesList;
 row.names(data) = NULL;
 Encoding(data[, 2]) = "UTF-8";
 Encoding(data[, 5]) = "UTF-8";
-print(data);
+
+write.xlsx(data, file = "1_0.xlsx", row.names = FALSE);
